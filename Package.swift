50887e4722edda9209f2a65a9d1c847ad9f988c7
laiazxyq <laiazxyq@gmail.com>
2025-08-03 16:48:17 +0800

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "libcmark_extend",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "libcmark_extend",
+            targets: ["libcmark_extend"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "libcmark_extend"),
+        .testTarget(
+            name: "libcmark_extendTests",
+            dependencies: ["libcmark_extend"]
+        ),
+    ]
+)
